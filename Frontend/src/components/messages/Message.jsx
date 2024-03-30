92177e1f8de09cb5b8ac489db8139fae7d9d9962
sachin kunwar <sachinkunwar284@gmail.com>
2024-03-30 21:42:25 +0545

    4th commit

--- Frontend/src/components/messages/Message.jsx
+++ Frontend/src/components/messages/Message.jsx
@@ -11,7 +11,6 @@ const Message = ({ message }) => {
 	const chatClassName = fromMe ? "chat-end" : "chat-start";
 	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
 	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
-
 	const shakeClass = message.shouldShake ? "shake" : "";
 
 	return (
